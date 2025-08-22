1a8d20ba91eccad78ea6cb0cb00b42a5db72018c
sahipe <sahipe11@gmail.com>
2025-08-22 12:56:27 +0530

    excel file name fixed.

--- client/src/components/MetsOrLeadsDetailsExcelDownload.jsx
+++ client/src/components/MetsOrLeadsDetailsExcelDownload.jsx
@@ -57,7 +57,7 @@ const MetsOrLeadsDetailsExcelDownload = () => {
       const url = window.URL.createObjectURL(new Blob([res.data]));
       const link = document.createElement("a");
       link.href = url;
-      link.setAttribute("download", "customers_data.xlsx");
+      link.setAttribute("download", "Mets_or_Leads_details_data.xlsx");
       document.body.appendChild(link);
       link.click();
       link.remove();
